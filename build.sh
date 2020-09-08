2b5c5965201b4f0934bc9d9c094ad44540559325
Junbum Lee <jun@beomi.net>
2020-09-08 12:08:29 +0900

    Add tagging with 'latest'

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 export UID=$(id -u)
 export GID=$(id -g)
-docker build -t beomi/transformers-pytorch-gpu:1.7.0 . \
-           --build-arg UID=$UID  
+DOCKER_VERSION=1.7.0
+docker build -t beomi/transformers-pytorch-gpu:$DOCKER_VERSION . \
+           --build-arg UID=$UID 
+docker tag beomi/transformers-pytorch-gpu:$DOCKER_VERSION \
+           beomi/transformers-pytorch-gpu:latest
