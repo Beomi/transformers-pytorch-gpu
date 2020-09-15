8d9bec7320f647bdc132971a58229e2d6da1e34c
Junbum Lee <jun@beomi.net>
2020-09-15 22:39:53 +0900

    Use cuda 10.1 for pytorch 1.6.0

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 docker stop transformers-ex
 docker rm transformers-ex
-docker run -d --gpus=all \
+docker run -d --gpus=all --ipc=host \
     --name=transformers-ex \
     --restart=unless-stopped \
     -u $(id -u ${USER}):$(id -g ${USER}) \
     -p 42000:8888 \
     -v $(pwd):/home/user/code \
-    beomi/transformers-pytorch-gpu:1.7.0 \
+    beomi/transformers-pytorch-gpu:1.6.0 \
     zsh -c '
     export SHELL=zsh;
     jupyter notebook \
