5121d40c8f76e833724609a75f64fa17cd1097ee
Junbum Lee <jun@beomi.net>
2020-09-23 10:21:59 +0900

    Remove latest tagging

--- 1.6.0/build.sh
+++ 1.6.0/build.sh
@@ -3,4 +3,3 @@ export GID=$(id -g)
 DOCKER_VERSION=1.6.0
 docker build -t beomi/transformers-pytorch-gpu:$DOCKER_VERSION . \
            --build-arg UID=$UID 
-docker tag beomi/transformers-pytorch-gpu:$DOCKER_VERSION
--- 1.7.0-nvcr/build.sh
+++ 1.7.0-nvcr/build.sh
@@ -3,4 +3,3 @@ export GID=$(id -g)
 DOCKER_VERSION="1.7.0-nvcr"
 docker build -t beomi/transformers-pytorch-gpu:$DOCKER_VERSION . \
            --build-arg UID=$UID 
-docker tag beomi/transformers-pytorch-gpu:$DOCKER_VERSION
