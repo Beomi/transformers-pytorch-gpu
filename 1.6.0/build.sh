92aedb39bf75241bdac54fd22734e152b797202a
beomi <beomi@snu.ac.kr>
2020-11-12 18:35:27 +0900

    Change docker img to PyTorch Official

--- 1.6.0/build.sh
+++ 1.6.0/build.sh
@@ -1,5 +1,3 @@
-export UID=$(id -u)
-export GID=$(id -g)
 DOCKER_VERSION=1.6.0
 docker build -t beomi/transformers-pytorch-gpu:$DOCKER_VERSION . \
-           --build-arg UID=$UID 
+           --build-arg UID=$(id -u)
