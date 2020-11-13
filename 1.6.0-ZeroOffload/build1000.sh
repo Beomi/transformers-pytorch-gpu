3929276b651d92621fa59b9f19f507fad2f6827e
Junbum Lee <jun@beomi.net>
2020-11-13 15:58:21 +0900

    Add 1.6.0-ZeroOffload, Deepspeed ZeRO installed

---
+++ 1.6.0-ZeroOffload/build1000.sh
@@ -0,0 +1,5 @@
+DOCKER_VERSION=1.6.0-ZeroOffload
+TODAY=$(date +%Y%m%d)
+echo "Version: $DOCKER_VERSION.$TODAY"
+docker build -t beomi/transformers-pytorch-gpu:$DOCKER_VERSION.$TODAY . \
+           --build-arg UID=1000
